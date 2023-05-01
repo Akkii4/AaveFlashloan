9a44ec0cf4f4e6401f6206d0892f9bc742ecf60d
Akshit Choudhary <akshitchoudhary007@gmail.com>
2023-05-01 10:56:28 +0530

    Create Arbitrage.sol

---
+++ contracts/Arbitrage.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Arbitrage is Ownable {
+    using SafeERC20 for ERC20;
+    event ArbitrageCompleted(address doer, uint doerBalance, uint amountDone);
+
+    uint public minRequiredFunds;
+
+    constructor(uint _minRequiredFunds) {
+        minRequiredFunds = _minRequiredFunds;
+    }
+
+    function performArbitrage(address _tokenAddress) external {
+        address operator = msg.sender;
+        uint operatorBalance = IERC20(_tokenAddress).balanceOf(
+            address(operator)
+        );
+        require(operatorBalance >= minRequiredFunds, "Not enough funds");
+        uint256 _tokenBalance = IERC20(_tokenAddress).balanceOf(address(this));
+        SafeERC20.safeTransfer(IERC20(_tokenAddress), operator, _tokenBalance);
+        emit ArbitrageCompleted(operator, operatorBalance, _tokenBalance);
+    }
+
+    function updateMinRequiredFunds(uint _minRequiredFunds) external onlyOwner {
+        minRequiredFunds = _minRequiredFunds;
+    }
+}
